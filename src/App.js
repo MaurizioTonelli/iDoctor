b9dc955620a561a00288a597ddfdcbf97b081ecb
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2021-06-24 21:38:54 -0500

    Hash Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./globals.css";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard.js";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
-      <div>Bienvenido a iDoctor</div>
-      <Router>
-        <Switch>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
     </>
   );
 }
