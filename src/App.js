d7fc136a8bdb75e4e35c6067161172d20b5eaf05
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2021-06-24 21:29:35 -0500

    Hash routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard.js";
 import Login from "./pages/Login/Login";
 
